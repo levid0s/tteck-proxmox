03e448927129f96a983f25e457e044445bd6631a
levid0s <lev@bitl.ma>
2025-02-07 13:12:43 +0000

    tidy up + deactivated remote updates

--- install/daemonsync-install.sh
+++ install/daemonsync-install.sh
@@ -20,16 +20,16 @@ $STD apt-get install -y mc
 $STD apt-get install -y g++-multilib
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Daemon Sync Server"
-wget -qL https://github.com/community-scripts/ProxmoxVE/raw/main/misc/daemonsync_2.2.0.0059_amd64.deb
-$STD dpkg -i daemonsync_2.2.0.0059_amd64.deb
-msg_ok "Installed Daemon Sync Server"
+msg_info "Installing Daemon Sync Server [Deactivated by levid0s until security analysis]"
+# wget -qL https://github.com/com unity-scripts/ProxmoxVE/raw/main/misc/daemonsync_2.2.0.0059_amd64.deb
+# $STD dpkg -i daemonsync_2.2.0.0059_amd64.deb
+# msg_ok "Installed Daemon Sync Server"
 
 motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -rf daemonsync_2.2.0.0059_amd64.deb
+# rm -rf daemonsync_2.2.0.0059_amd64.deb
 $STD apt-get autoremove >/dev/null
 $STD apt-get autoclean >/dev/null
 msg_ok "Cleaned"
--- misc/cron-update-lxcs.sh
+++ misc/cron-update-lxcs.sh
@@ -17,23 +17,25 @@ cat <<"EOF"
 EOF
 
 add() {
-  while true; do
-    read -p "This script will add a crontab schedule that updates all LXCs every Sunday at midnight. Proceed(y/n)?" yn
-    case $yn in
-    [Yy]*) break ;;
-    [Nn]*) exit ;;
-    *) echo "Please answer yes or no." ;;
-    esac
-  done
-  sh -c '(crontab -l -u root 2>/dev/null; echo "0 0 * * 0 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin /bin/bash -c \"\$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/update-lxcs-cron.sh)\" >>/var/log/update-lxcs-cron.log 2>/dev/null") | crontab -u root -'
-  clear
-  echo -e "\n To view Cron Update LXCs logs: cat /var/log/update-lxcs-cron.log"
+  echo "community-scripts/ProxmoxVE updater disabled by levid0s until security review."
+  # while true; do
+  #   read -p "This script will add a crontab schedule that updates all LXCs every Sunday at midnight. Proceed(y/n)?" yn
+  #   case $yn in
+  #   [Yy]*) break ;;
+  #   [Nn]*) exit ;;
+  #   *) echo "Please answer yes or no." ;;
+  #   esac
+  # done
+  # sh -c '(crontab -l -u root 2>/dev/null; echo "0 0 * * 0 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin /bin/bash -c \"\$(wget -qLO - https://github.com/comm unity-scripts/ProxmoxVE/raw/main/misc/update-lxcs-cron.sh)\" >>/var/log/update-lxcs-cron.log 2>/dev/null") | crontab -u root -'
+  # clear
+  # echo -e "\n To view Cron Update LXCs logs: cat /var/log/update-lxcs-cron.log"
 }
 
 remove() {
-  (crontab -l | grep -v "github.com/community-scripts/ProxmoxVE/raw/main/misc/update-lxcs-cron.sh") | crontab -
-  rm -rf /var/log/update-lxcs-cron.log
-  echo "Removed Crontab Schedule from Proxmox VE"
+  # (crontab -l | grep -v "github.com/comm unity-scripts/ProxmoxVE/raw/main/misc/update-lxcs-cron.sh") | crontab -
+  # rm -rf /var/log/update-lxcs-cron.log
+  # echo "Removed Crontab Schedule from Proxmox VE"
+  echo "community-scripts/ProxmoxVE updater disabled by levid0s until security review."
 }
 
 OPTIONS=(Add "Add Crontab Schedule"
